382a157ebd6a8bd6b012070d5db60759d5d8d383
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-07-20 08:02:47 +0800

    nah

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@
 # Fake Signed
 # java -jar tool/uber-apk-signer.jar -a app/build/outputs/apk/release/app-release-unsigned.apk
 
-# java -jar lspatch.jar apk/tes.apk -m app/build/outputs/apk/release/app-release-aligned-debugSigned.apk -l 2
+# java -jar tool/lspatch.jar apk/28_raw.apk -m app/build/outputs/apk/release/app-release-aligned-debugSigned.apk -l 2
