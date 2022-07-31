de4d55a17c602201984ee97400d6f72ce871b076
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-08-01 00:26:54 +0800

    add libIl2CppHook

---
+++ app/src/main/java/yuuki/yuukips/Hook.kt
@@ -0,0 +1,369 @@
+package yuuki.yuukips
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.res.XModuleResources
+import android.graphics.Color
+import android.graphics.PixelFormat
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RoundRectShape
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.util.Base64
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowManager
+import android.webkit.SslErrorHandler
+import android.widget.*
+import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
+import com.github.kyuubiran.ezxhelper.utils.*
+import de.robv.android.xposed.IXposedHookZygoteInit
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+import org.json.JSONObject
+import yuuki.yuukips.Utils.dp2px
+import yuuki.yuukips.Utils.isInit
+import java.io.BufferedReader
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import java.util.regex.Pattern
+import javax.net.ssl.*
+import kotlin.system.exitProcess
+
+class Hook {
+    private val regex = Pattern.compile("http(s|)://.*?\\.(hoyoverse|mihoyo|yuanshen|mob)\\.com")
+
+    private lateinit var server: String
+
+    private lateinit var modulePath: String
+    private lateinit var moduleRes: XModuleResources
+    private lateinit var windowManager: WindowManager
+
+    private val activityList: ArrayList<Activity> = arrayListOf()
+    private var activity: Activity
+        get() {
+            for (mActivity in activityList) {
+                if (mActivity.isFinishing) {
+                    activityList.remove(mActivity)
+                } else {
+                    return mActivity
+                }
+            }
+            throw Throwable("Activity not found.")
+        }
+        set(value) {
+            activityList.add(value)
+        }
+
+    private fun getDefaultSSLSocketFactory(): SSLSocketFactory {
+        return SSLContext.getInstance("TLS").apply {
+            init(arrayOf<KeyManager>(), arrayOf<TrustManager>(DefaultTrustManager()), SecureRandom())
+        }.socketFactory
+    }
+
+    private fun getDefaultHostnameVerifier(): HostnameVerifier {
+        return DefaultHostnameVerifier()
+    }
+
+    class DefaultHostnameVerifier : HostnameVerifier {
+        @SuppressLint("BadHostnameVerifier")
+        override fun verify(p0: String?, p1: SSLSession?): Boolean {
+            return true
+        }
+
+    }
+
+    @SuppressLint("CustomX509TrustManager")
+    private class DefaultTrustManager : X509TrustManager {
+
+        @SuppressLint("TrustAllX509TrustManager")
+        override fun checkClientTrusted(chain: Array<X509Certificate?>?, authType: String?) {
+        }
+
+        @SuppressLint("TrustAllX509TrustManager")
+        override fun checkServerTrusted(chain: Array<X509Certificate?>?, authType: String?) {
+        }
+
+        override fun getAcceptedIssuers(): Array<X509Certificate> {
+            return arrayOf()
+        }
+    }
+
+    fun initZygote(startupParam: IXposedHookZygoteInit.StartupParam) {
+        modulePath = startupParam.modulePath
+        moduleRes = XModuleResources.createInstance(modulePath, null)
+        TrustMeAlready().initZygote()
+
+        // default
+        server = ""
+    }
+
+    private var startForceUrl = false
+    private var startProxyList = false
+    private lateinit var dialog: LinearLayout
+
+    @SuppressLint("WrongConstant", "ClickableViewAccessibility")
+    fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        if (lpparam.packageName != "com.miHoYo.GenshinImpact") return
+
+        EzXHelperInit.initHandleLoadPackage(lpparam)
+
+        findMethod(Activity::class.java, true) { name == "onCreate" }.hookBefore { param ->
+            activity = param.thisObject as Activity
+        }
+        findMethod("com.miHoYo.GetMobileInfo.MainActivity") { name == "onCreate" }.hookBefore { param ->
+            activity = param.thisObject as Activity
+            showDialog()
+        }
+    }
+
+    private fun httpUtils(url: String, mode: String = "GET", data: String = "", callback: (HttpURLConnection, String) -> Unit) {
+        var ret: String
+        URL("$server$url").apply {
+            val conn = if (server.startsWith("https")) {
+                (openConnection() as HttpsURLConnection).apply {
+                    sslSocketFactory = getDefaultSSLSocketFactory()
+                    hostnameVerifier = getDefaultHostnameVerifier()
+                }
+            } else {
+                openConnection() as HttpURLConnection
+            }.apply {
+                requestMethod = mode
+                readTimeout = 8000
+                connectTimeout = 8000
+                if (mode == "POST") {
+                    doOutput = true
+                    doInput = true
+                    useCaches = false
+                    outputStream.apply {
+                        write(data.toByteArray())
+                        flush()
+                    }
+                    val input = inputStream
+                    val message = ByteArrayOutputStream()
+
+                    var len: Int
+                    val buffer = ByteArray(1024)
+                    while (input.read(buffer).also { len = it } != -1) {
+                        message.write(buffer, 0, len)
+                    }
+                    input.close()
+                    message.close()
+                    ret = String(message.toByteArray())
+                } else {
+                    val response = StringBuilder()
+                    var line = ""
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+                    while (reader.readLine()?.also { line = it } != null) {
+                        response.append(line)
+                    }
+                    ret = response.toString()
+                }
+            }
+            callback(conn, ret)
+        }
+    }
+
+    private fun showDialog() {
+        AlertDialog.Builder(activity).apply {
+            setCancelable(false)
+            setTitle("Welcome to YuukiPS")
+            setMessage("To connect with us please select server region and for official servers use Custom Server with blank input, fill in if you want to connect to another server. info: discord.yuuki.me")
+
+            setView(ScrollView(context).apply {
+
+            addView(EditText(activity).apply {
+                val str = ""
+                setText(str.toCharArray(), 0, str.length)
+                addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(p0: CharSequence, p1: Int, p2: Int, p3: Int) {}
+                    override fun onTextChanged(p0: CharSequence, p1: Int, p2: Int, p3: Int) {}
+                    @SuppressLint("CommitPrefEdits")
+                    override fun afterTextChanged(p0: Editable) {
+                        server = p0.toString()
+                    }
+                })
+            })
+            
+            })
+
+            // TODO: add patch metadata
+            // TODO: remove patch metadata
+
+            // Yuuki
+            setNegativeButton("Singapore") { _, _ ->
+                server = "https://sg.game.yuuki.me"
+                hook()
+                Toast.makeText(activity, "Welcome to Singapore Region", Toast.LENGTH_LONG).show()
+            }
+            setPositiveButton("German") { _, _ ->
+                server = "https://de.game.yuuki.me"
+                hook()
+                Toast.makeText(activity, "Welcome to German Region", Toast.LENGTH_LONG).show()
+            }
+            setNeutralButton("Custom Server") { _, _ ->                
+                if (server != ""){
+                    hook()
+                    Toast.makeText(activity, "You are currently connected to server: "+server, Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(activity, "You are currently connected to an official server, use it to download data only.", Toast.LENGTH_LONG).show()
+                }
+                //activity.finish() // use this to close?                
+            }
+
+        }.show()
+    }
+
+    inner class MoveOnTouchListener : View.OnTouchListener {
+        private var originalXPos = 0
+        private var originalYPos = 0
+
+        private var offsetX = 0f
+        private var offsetY = 0f
+
+        @SuppressLint("ClickableViewAccessibility")
+        override fun onTouch(v: View, event: MotionEvent): Boolean {
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    val x = event.rawX
+                    val y = event.rawY
+
+                    val location = IntArray(2)
+                    v.getLocationOnScreen(location)
+
+                    originalXPos = location[0]
+                    originalYPos = location[1]
+
+                    offsetX = x - originalXPos
+                    offsetY = y - originalYPos
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    val onScreen = IntArray(2)
+                    v.getLocationOnScreen(onScreen)
+
+                    val x = event.rawX
+                    val y = event.rawY
+
+                    val params: WindowManager.LayoutParams = v.layoutParams as WindowManager.LayoutParams
+
+                    val newX = (x - offsetX).toInt()
+                    val newY = (y - offsetY).toInt()
+
+                    if (newX == originalXPos && newY == originalYPos) {
+                        return false
+                    }
+
+                    params.x = newX
+                    params.y = newY
+
+                    windowManager.updateViewLayout(v, params)
+                }
+            }
+            return false
+        }
+    }
+
+    private fun sslHook() {
+        // OkHttp3 Hook
+        findMethodOrNull("com.combosdk.lib.third.okhttp3.OkHttpClient\$Builder") { name == "build" }?.hookBefore {
+            it.thisObject.invokeMethod("sslSocketFactory", args(getDefaultSSLSocketFactory()), argTypes(SSLSocketFactory::class.java))
+            it.thisObject.invokeMethod("hostnameVerifier", args(getDefaultHostnameVerifier()), argTypes(HostnameVerifier::class.java))
+        }
+        findMethodOrNull("okhttp3.OkHttpClient\$Builder") { name == "build" }?.hookBefore {
+            it.thisObject.invokeMethod("sslSocketFactory", args(getDefaultSSLSocketFactory(), DefaultTrustManager()), argTypes(SSLSocketFactory::class.java, X509TrustManager::class.java))
+            it.thisObject.invokeMethod("hostnameVerifier", args(getDefaultHostnameVerifier()), argTypes(HostnameVerifier::class.java))
+        }
+        // WebView Hook
+        arrayListOf(
+            "android.webkit.WebViewClient",
+            "cn.sharesdk.framework.g",
+            "com.facebook.internal.WebDialog\$DialogWebViewClient",
+            "com.geetest.sdk.dialog.views.GtWebView\$c",
+            "com.miHoYo.sdk.webview.common.view.ContentWebView\$6"
+        ).forEach {
+            findMethodOrNull(it) { name == "onReceivedSslError" && parameterTypes[1] == SslErrorHandler::class.java }?.hookBefore { param ->
+                (param.args[1] as SslErrorHandler).proceed()
+            }
+        }
+        // Android HttpsURLConnection Hook
+        findMethodOrNull("javax.net.ssl.HttpsURLConnection") { name == "getDefaultSSLSocketFactory" }?.hookBefore {
+            it.result = getDefaultSSLSocketFactory()
+        }
+        findMethodOrNull("javax.net.ssl.HttpsURLConnection") { name == "setSSLSocketFactory" }?.hookBefore {
+            it.result = null
+        }
+        findMethodOrNull("javax.net.ssl.HttpsURLConnection") { name == "setDefaultSSLSocketFactory" }?.hookBefore {
+            it.result = null
+        }
+        findMethodOrNull("javax.net.ssl.HttpsURLConnection") { name == "setHostnameVerifier" }?.hookBefore {
+            it.result = null
+        }
+        findMethodOrNull("javax.net.ssl.HttpsURLConnection") { name == "setDefaultHostnameVerifier" }?.hookBefore {
+            it.result = null
+        }
+        findMethodOrNull("javax.net.ssl.HttpsURLConnection") { name == "getDefaultHostnameVerifier" }?.hookBefore {
+            it.result = getDefaultHostnameVerifier()
+        }
+    }
+
+    private fun hook() {
+        findMethod("com.miHoYo.sdk.webview.MiHoYoWebview") { name == "load" && parameterTypes[0] == String::class.java && parameterTypes[1] == String::class.java }.hookBefore {
+            replaceUrl(it, 1)
+        }
+        findAllMethods("android.webkit.WebView") { name == "loadUrl" }.hookBefore {
+            replaceUrl(it, 0)
+        }
+        findAllMethods("android.webkit.WebView") { name == "postUrl" }.hookBefore {
+            replaceUrl(it, 0)
+        }
+
+        findMethod("okhttp3.HttpUrl") { name == "parse" && parameterTypes[0] == String::class.java }.hookBefore {
+            replaceUrl(it, 0)
+        }
+        findMethod("com.combosdk.lib.third.okhttp3.HttpUrl") { name == "parse" && parameterTypes[0] == String::class.java }.hookBefore {
+            replaceUrl(it, 0)
+        }
+
+        findMethod("com.google.gson.Gson") { name == "fromJson" && parameterTypes[0] == String::class.java && parameterTypes[1] == java.lang.reflect.Type::class.java }.hookBefore {
+            replaceUrl(it, 0)
+        }
+        findConstructor("java.net.URL") { parameterTypes[0] == String::class.java }.hookBefore {
+            replaceUrl(it, 0)
+        }
+        findMethod("com.combosdk.lib.third.okhttp3.Request\$Builder") { name == "url" && parameterTypes[0] == String::class.java }.hookBefore {
+            replaceUrl(it, 0)
+        }
+        findMethod("okhttp3.Request\$Builder") { name == "url" && parameterTypes[0] == String::class.java }.hookBefore {
+            replaceUrl(it, 0)
+        }
+    }
+
+    private fun replaceUrl(method: XC_MethodHook.MethodHookParam, args: Int) {
+        
+        if (server == "") return
+        if (method.args[args].toString() == "") return
+
+        //XposedBridge.log("old: " + method.args[args].toString())
+        if (method.args[args].toString().startsWith("[{\"area\":")) return
+
+        val m = regex.matcher(method.args[args].toString())
+        if (m.find()) {
+         method.args[args] = m.replaceAll(server)
+        }
+        //XposedBridge.log("new: " + method.args[args].toString())
+    }
+}
--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 # Make Release
 ./gradlew assembleRelease
-
+# ./gradlew clean build https://stackoverflow.com/questions/34005713/difference-between-clean-gradlew-clean
 # Fake Signed
 # java -jar tool/uber-apk-signer.jar -a app/build/outputs/apk/release/app-release-unsigned.apk
 
-# java -jar tool/lspatch.jar apk/28_raw.apk -m app/build/outputs/apk/release/app-release-aligned-debugSigned.apk -l 2
+# java -jar tool/lspatch.jar apk/GenshinImpact_v2.8.0_mod.apk -m app/build/outputs/apk/release/app-release-aligned-debugSigned.apk --v2
